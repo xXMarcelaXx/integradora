47766257e3cb36cc1fd2025ab69b5ee63031dc3d
carlosdr14 <107272990+carlosdr14@users.noreply.github.com>
2022-08-17 14:59:37 -0500

    Update ejecutal.php

--- src/query/ejecutal.php
+++ src/query/ejecutal.php
@@ -18,23 +18,23 @@ class ejecutal
         }
         catch(PDOException $e)
         {
-            echo"<p>nombre de usuario en uso</p>";
+            echo"<p>nombre de usuario o correo en uso</p>";
         }
     }
 
-    public function verificareg($nombre_usuario)
+    public function verificareg($nombre_usuario,$correo)
     {
         try
         {
             $pase=0;
-            $cc= new Database("barberia","root","admin");
+            $cc= new Database("barberia","bar","admin");
             $objetopdo=$cc->getPDO();
 
-            $query="SELECT* FROM cuenta WHERE nombre_usuario='$nombre_usuario'";
+            $query="SELECT* FROM cuenta WHERE nombre_usuario='$nombre_usuario' or correo='$correo'";
             $consulta=$objetopdo->query($query);
             while($renglon=$consulta->fetch(PDO::FETCH_ASSOC))
             {
-                if ($nombre_usuario=$renglon['nombre_usuario']) {
+                if ($nombre_usuario=$renglon['nombre_usuario'] or $correo=$renglon['correo']) {
                     $pase=1;
                 }
                 else {
@@ -46,14 +46,14 @@ class ejecutal
             {
                 
                 echo"<div class='alert alert-danger'>";
-                echo"<h2 align='center'>Usuario o correo ya registrados</h2>";
+               echo"<h2 align='center'><font color='red'>Usuario o correo ya existentes</font></h2>";
                 echo"</div>";
                 header("refresh:2 ../../views/registro.php");
             }
             else
             {
                 echo"<div class='alert alert-success'>";
-                echo"<p align='center'><font color='green'>usuario registrado</font></p>";
+                echo"<h1 align='center'><font color='green'>usuario registrado</font></h1>";
                 echo"</div>";
             header("location:../../views/iniciosesion.php");
             }      
