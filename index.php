26258a6fe5ec64d8faf7610ab9b0312e1c9d926e
xXMarcelaXx <110473185+xXMarcelaXx@users.noreply.github.com>
2022-08-17 14:27:56 -0500

    dsa

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="shortcut icon" href="../img/logo.png">
-    <link rel="stylesheet" href="../integradora/css/estilo1.css">
-	<link rel="stylesheet" href="../integradora/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../integradora/css/fontello.css">
+	<link rel="shortcut icon" href="img/logo.png">
+    <link rel="stylesheet" href="css/estilo1.css">
+	<link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href="css/fontello.css">
     <title>classic cuts</title>
 </head>
 <body>
