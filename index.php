f4d888de79fac0db2ef460bce70decd88c6a80cc
xXMarcelaXx <110473185+xXMarcelaXx@users.noreply.github.com>
2022-08-17 14:29:46 -0500

    img index

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="shortcut icon" href="img/logo.png">
+	<link rel="shortcut icon" href="img/logo.jpg">
     <link rel="stylesheet" href="css/estilo1.css">
 	<link rel="stylesheet" href="bootstrap.min.css">
     <link rel="stylesheet" href="css/fontello.css">
