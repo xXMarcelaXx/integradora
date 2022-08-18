3e1ae527ca3d9b4800b27073a5a9f9d9f2ee10ef
xXMarcelaXx <110473185+xXMarcelaXx@users.noreply.github.com>
2022-08-17 19:38:36 -0500

    Update quejas.php

--- views/scripts/quejas.php
+++ views/scripts/quejas.php
@@ -6,7 +6,7 @@ $insert= new ejecuta();
 session_start();
 extract($_POST);
 $cadena= "INSERT INTO quejas VALUES ('','$message','".$_SESSION['usuario']."')";
-
+   
  $insert->ejecutar($cadena);
   echo "<script> alert('Gracias');</script>";
    header ("refresh:3;  ../../views/profile2.php#contactanos");
