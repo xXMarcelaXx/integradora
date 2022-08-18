c8c896ef8605fcdb30e6c2658b46f8b04c512c4d
xXMarcelaXx <110473185+xXMarcelaXx@users.noreply.github.com>
2022-08-17 19:24:38 -0500

    cattt

--- views/scripts/actualizarcat.php
+++ views/scripts/actualizarcat.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../css/bootstrap.min.css">
-
-    <title>Actualiza Cat</title>
-</head>
-<body>
-    <div class="container">
-
-    <?php
+ <?php
     use barber\query\ejecuta;
     require("../../vendor/autoload.php");
 
@@ -24,11 +11,6 @@ $cadena="UPDATE cat_productos SET categoria='$categoria'
 
     $insert->ejecutar($cadena);
 
-        header("refresh:2; ../verCat.php");
+        header("location:../verCat.php");
 
     ?>
-
-    </div>
-    
-</body>
-</html>
