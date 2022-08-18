f7da94c3718e2aac401cfd5551e8721b89c5ef67
carlosdr14 <107272990+carlosdr14@users.noreply.github.com>
2022-08-18 18:11:35 -0500

    Update Usuario.php

--- views/Usuario.php
+++ views/Usuario.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="../img/logo.jpg">
     <link rel="stylesheet" href="../css/admin.css">
+    <link rel="stylesheet" href="../css/sesion1.css">
     <link rel="stylesheet" href="../js/bootstrap.bundle.js">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="../css/bootstrap.min.css">
