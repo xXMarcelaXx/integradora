80c91a8df32c0eb01899efc06d92f9614251d1f8
AbelardoReyes <109323948+AbelardoReyes@users.noreply.github.com>
2022-08-18 19:34:54 -0500

    Update modificarcitaadmin.php

--- views/scripts/modificarcitaadmin.php
+++ views/scripts/modificarcitaadmin.php
@@ -25,7 +25,7 @@
     
 
     $cita = new citas();
-    $cita->CITAADMIN($servicio, $horario);
+    $cita->CITACLIENTE($servicio, $horario);
 
     header("refresh:3; ../vistaadmin.php");
     echo "<h1>Cita Modificada</h1>";
