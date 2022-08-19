07f363b10583c02a0e598544ac14e906f590539a
AbelardoReyes <109323948+AbelardoReyes@users.noreply.github.com>
2022-08-18 22:56:42 -0500

    intento #11

--- src/query/citas.php
+++ src/query/citas.php
@@ -80,7 +80,7 @@ class citas
         $servq = "UPDATE servicio_cita set servicio_sc = $servicio WHERE id_ovcita = " . intval($_SESSION['id_ovcita']) . "";
         $reg = $servicioup->seleccionar($servq);
         $servicioup1 = new select();
-        $servq1 = "UPDATE citas set hora_cita = $horario WHERE id_citas=". $_SESSION['foliocita'] ."";
+        $servq1 = "UPDATE citas set hora_cita = $horario WHERE id_citas=". intval($_SESSION['foliocita']) ."";
         $reg1 = $servicioup1->seleccionar($servq1);
         
     }
